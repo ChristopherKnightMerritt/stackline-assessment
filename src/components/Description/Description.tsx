1cc9db605be54db93b5b2d55cb10bb6f58ec78a2
Christopher Merritt <christophermerritt@Christophers-MacBook-Air.local>
2024-04-16 15:56:15 -0700

    build out description component

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,9 +1,40 @@
 import './Description.css'
 
-const Description = () => {
+interface Props {
+    productDetails: Array<productDetail>,
+}
+interface productDetail {
+    brand: string,
+    details: Array<string>,
+    id: string,
+    image: string,
+    retailer: string,
+    tags: Array<string>,
+    title: string,
+    subtitle: string
+}
+
+const Description: React.FC<Props> = ({productDetails}) => {
+
+    const product = productDetails[0];
 
     return (
-        <div className='description-container'>Description</div>
+        <div className='description-container'>
+            <div className='product-info-container'>
+                <img src={product.image} alt={product.title} />
+                <h3 className='product-name'>{product.title}</h3>
+                <p>{product.subtitle}</p>
+            </div>
+            <div className='tag-container'>
+                {
+                    product.tags.map((tag: string, i) => {
+                        return(
+                            <div className='tag' key={i} >{tag}</div>
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
 
--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -7,21 +7,30 @@ import Graph from "../Graph/Graph";
 import Datatable from "../Datatable/Datatable";
 import './Homepage.css'
 
+interface productDetail {
+    brand: string,
+    details: Array<string>,
+    id: string,
+    image: string,
+    retailer: string,
+    tags: Array<string>,
+    title: string,
+    subtitle: string
+}
 
 const Homepage = () => {
 
     const dispatch: AppDispatch = useAppDispatch();
-
-    
     dispatch(loadProduct())
       
+    const allProductData = useSelector((state: RootState) => state.product);
+    const productData = allProductData['product']
+    const productDetail = productData as productDetail[]
 
-    const productData = useSelector((state: RootState) => state.product);
-    console.log('productData: ', productData)
     return (<>
         <Header />
         <div className="body-container">
-            <Description />
+            <Description productDetails= {productDetail}/>
             <div className="vertical-column">
                 <Graph />
                 <Datatable />
